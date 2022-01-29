1a717d6f80c618eedfcc393eb8b6353f51f22915
kramstyles <mark.eke@decagon.dev>
2022-01-29 23:53:44 +0100

    Added a global variable python file

---
+++ Python/morse_code.py
@@ -0,0 +1,18 @@
+def morse(morse_code):
+    """
+    The Morse code encodes every character as a sequence of "dots" and "dashes".
+
+    For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−.
+
+    The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words.
+
+    For example, the message Decadev in Morse code is -.. . -.-. .- -.. . ...-
+
+    NOTE: Extra spaces before or after the code have no meaning and should be ignored. In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words. Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+
+    param = morse_code : A string of morse codes
+    return word : Converted value of morse code to string
+    """
+
+    
+    pass
