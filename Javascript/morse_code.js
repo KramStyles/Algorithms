80355dd63aefb4dc6cc53b4dbbcef9d8065ed5f4
kramstyles <mark.eke@decagon.dev>
2022-01-30 09:45:46 +0100

    Fixed morse code in both python and javascript

--- Javascript/morse_code.js
+++ Javascript/morse_code.js
@@ -54,17 +54,32 @@ const MORSE_CODE = {
     "--..": "Z",
     "..--.-": "_",
     "...---...": "SOS",
-  };
+};
 
-  test1 = "-.. . -.-. .- -.. . ...- ..."
-  test2 = "-.. . -.-. .- -.. . ...-   ..."
+test1 = "-.. . -.-. .- -.. . ...- ..."
+test2 = "-.. . -.-. .- -.. . ...-   ..."
 
-  function morse(code) {
-      code = code.split(' ');
-      word = "";
-      code.forEach(symbol => {
-         word += MORSE_CODE[symbol] 
-      });
-      return word;
+function morse(code) {
+    if (code.search('   ') > -1) {
+        word = '';
+        code = code.split('   ');
+        code.forEach(words => {
+            // debugger;
+            let initial = "";
+            words = words.split(' ')
+            words.forEach(letter => {
+                initial += MORSE_CODE[letter];
+            });
+            word += (initial + " ");
+        });
+    } else {
+        code = code.split(' ');
+        word = "";
+        code.forEach(symbol => {
+            word += MORSE_CODE[symbol]
+        });
+    }
 
-  }
+    return word.trim();
+
+}
