d37e1fa54915db836f14aaeecfdf3b49f175ce13
kramstyles <mark.eke@decagon.dev>
2022-01-30 12:46:25 +0100

    Solved the isolate_duplicates.py algorithm

--- Python/isolate_duplicates.py
+++ Python/isolate_duplicates.py
@@ -1,2 +1,31 @@
 test = "aaaabbcdefffffffg"
 # ["aa[aa]bbcdeff[fffff]g", 2]
+
+def isolate(duplicates):
+    words = []
+    initial = []
+    hold = ""
+    counter = 0
+    # For loop to get the uniques of the letters
+    for x in duplicates:
+        if x not in initial:
+            initial.append(x)
+    # Loop to get similar characters in a single array
+    for x in initial:
+        for char in duplicates:
+            if char == x:
+                hold += x
+        words.append(hold)
+        hold = ""
+
+    for x in words:
+        if len(x) > 2:
+            hold += f"{x[:2]}[{x[2:]}]"
+            counter += 1
+        else:
+            hold += x
+
+    return [hold, counter]
+
+
+print(isolate("aaasssssssddddffgkkkkkuueyyryrhhsgfhjjsnnnnnnjjfmgjghjdjjjjdhyteghsjckjdheyebnfjhhgnjdfyfggbejjwtyjfjcbdgdhdjfjeeebddhdychdjdjmkj"))
