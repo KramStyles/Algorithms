dcaab06e168375299f956149b38c488a75cc3efb
kramstyles <mark.eke@decagon.dev>
2022-01-31 11:36:21 +0100

    started on fibonacci

--- Python/factorial.py
+++ Python/factorial.py
@@ -3,3 +3,13 @@ def normalFac(number):
     while number > 0:
         ans *= number
         number -= 1
+    return (ans)
+
+def recursiveFactorial(number):
+    if number > 0:
+        return (number * recursiveFactorial(number - 1))
+    else:
+        return False
+
+
+print(recursiveFactorial(5))
