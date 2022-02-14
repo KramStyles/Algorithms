b154ab13abb35fe2e3f0fdcbabbdc7eb87cc68fc
kramstyles <kramstyles@outlook.com>
2022-02-14 10:54:47 +0100

    fixed errors binary_con.py

--- Python/binary_conversions.py
+++ Python/binary_conversions.py
@@ -1,14 +1,19 @@
 def dec_to_bin(number: int):
     try:
-        return(bin(number[2:]))
+        return(bin(number)[2:])
     except Exception as err:
         return str(f"Problem occured: {err}")
 
 
 def bin_to_dec(number:str):
+    number = str(number)
     if 'b' in number:
         number = number[2:]
     try:
         return(int(number, 2))
     except Exception as err:
         return str(f"Problem occured: {err}")
+
+
+print(dec_to_bin(30))
+print(bin_to_dec(11110))
