110230c1ce7fbf4e4ea827db99ffbeec599e69dc
kramstyles <kramstyles@outlook.com>
2022-02-21 14:14:12 +0100

    shorten code

--- Python/array_packaging.py
+++ Python/array_packaging.py
@@ -3,10 +3,10 @@ def array_packing(integers: list) -> int:
     integers[0] = integers[-1]
     integers[-1] = temp
     numbs = [bin(x)[2:] for x in integers]
-    total = []
-    for x in numbs:
-      zer = '0'*(8-len(x))
-      total.append(zer+x)
+    # total = []
+    # for x in numbs:
+    #   zer = '0'*(8-len(x))
+    #   total.append(zer+x)
      
     total = [('0'*(8-len(x))) + x for x in numbs]
     return int(''.join(total), 2)
