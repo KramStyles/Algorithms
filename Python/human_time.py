9c78dc0499f08319f606662f2762029fc88e8d88
kramstyles <kramstyles@outlook.com>
2022-02-21 11:49:04 +0100

    finished human_time.py

---
+++ Python/human_time.py
@@ -0,0 +1,19 @@
+def human_time(seconds):
+    value = seconds / 3600
+    hours = int(value)
+    partial_mins = (value % 1) * 60
+    mins = int(partial_mins) 
+    secs = round((partial_mins % 1) * 60)
+
+    if hours < 10:
+        hours = f"0{hours}"
+    if mins < 10:
+        mins = f"0{mins}"
+    if secs < 10:
+        secs = f"0{secs}"
+    
+    return(f"{hours}:{mins}:{secs}")
+
+
+print(human_time(82874))
+print(human_time(5))
