2e82479b8f7e29230653e9e28be90ce3656f860a
kramstyles <mark.eke@decagon.dev>
2022-02-28 08:49:17 +0100

    solved myanagrams.py

---
+++ Python/my_anagrams.py
@@ -0,0 +1,44 @@
+"""
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters.
+ For example:
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+Note for Go
+For Go: Empty string slice is expected when there are no anagrams found.
+"""
+def anagrams(word, words):
+    answer = []
+    for wd in words:
+        if len(word) == len(wd):
+            count = 0
+            for char in wd:
+                if char in word:
+                    count+=1
+            if count == len(word):
+                answer.append(wd)
+
+    if answer:
+        for chars in answer:
+            for wd in word:
+                if wd not in chars:
+                    answer.remove(chars)
+    return answer
+
+def anagrams1(word, words):
+    from collections import Counter
+    counts = Counter(word)
+    return [w for w in words if Counter(w) == Counter(word)]
+
+# print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
+print(anagrams1('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))
+# print(anagrams('laser', ['lazing', 'lazy',  'lacer']))
