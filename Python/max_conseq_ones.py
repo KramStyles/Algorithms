1937971ffb5afb7c02596f39d883d8a78089bd8d
kramstyles <mark.eke@decagon.dev>
2022-02-28 15:46:52 +0100

    added extra code in max_cons.py

--- Python/max_conseq_ones.py
+++ Python/max_conseq_ones.py
@@ -22,5 +22,14 @@ def findMaxConsecutiveOnes(nums):
 test = [1,0]
 test1 = [1,0,1,1,1]
 
-print(findMaxConsecutiveOnes(test))
+# print(findMaxConsecutiveOnes(test))
 # print(findMaxConsecutiveOnes(test1))
+
+def findNumbers(nums):
+        return len([counter for counter in nums if len(str(counter)) % 2 == 0])
+        # counter = 0
+        # for num in nums:
+        #     if not len(str(num)) % 2: counter +=1
+        # return counter
+
+print(findNumbers([12,345,2,6,7896]))
