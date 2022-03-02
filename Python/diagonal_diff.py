7ef6f6b4da85ebaf359211808230eb46e54b22f4
kramstyles <mark.eke@decagon.dev>
2022-03-02 13:44:39 +0100

    solved diagonal_diff and calculate corner_sum

---
+++ Python/diagonal_diff.py
@@ -0,0 +1,67 @@
+from random import randint
+from ssl import match_hostname
+
+
+class Matrix:
+    def __init__(self):
+        self.matrix = []
+
+    def initialise_matrix(self, size):
+        """
+            Generates an n x n matrix based on the `size` passed in as parameter.
+            For example: if size = 3, then a 3 by 3 matrix would be generated.
+        """
+        for row in range(0,size):
+            self.matrix.append([])
+            for col in range(0,size):
+                value = randint(0,99)
+                self.matrix[row].append(value)
+        return self.matrix
+    
+    def display_matrix(self):
+        """
+            Displays the square matrix as a 2D grid.
+        """
+        length = len(self.matrix)
+        for row in range(0, length):
+            st = "| " 
+            for col in range(0, length):
+                if self.matrix[row][col] < 10:
+                    st = st + str(self.matrix[row][col]) + "  | "
+                else:
+                    st = st + str(self.matrix[row][col]) + " | "
+            print(st)
+
+    def calculate_diagonal_diff(self):
+        ans = []
+        back = []
+        ans2 = []
+        for index, mat in enumerate(self.matrix):
+            back = (len(self.matrix) - index) - 1
+            ans2.append(mat[back])
+            ans.append(mat[index])
+        print("Forward = ", ans)
+        print("Backward = ", ans2)
+        return ("The diagnonal difference is ", sum(ans2) - sum(ans) ) 
+
+
+    def calculate_corner_sum(self):
+        ans = []
+        for index, mat in enumerate(self.matrix):
+            if index == 0:
+                ans.append(mat[0])
+                ans.append(mat[-1])
+            if index == len(self.matrix)-1:
+                ans.append(mat[0])
+                ans.append(mat[-1])
+        print(ans)
+        return sum(ans)
+        
+
+
+
+matrix = Matrix()
+matrix.initialise_matrix(3)
+matrix.display_matrix()
+print(matrix.calculate_diagonal_diff())
+print(matrix.calculate_corner_sum())
