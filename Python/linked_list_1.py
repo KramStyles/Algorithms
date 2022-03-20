66367c53acc7bc8c2997ae59b20efa528a8c8d33
kramstyles <mark.eke@decagon.dev>
2022-03-20 10:04:04 +0100

    created insert and print function in linked list

--- Python/linked_list_1.py
+++ Python/linked_list_1.py
@@ -3,3 +3,22 @@ class Node:
         self.data = data
         self.next = nxt
         # This is created because a node is like a box that contains data and position to the next data
+
+class LinkedList:
+    def __init__(self) -> None:
+        # Head variable that points to the head of the list
+        self.head = None 
+
+    def insert_in_front(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print_link(self):
+        if not self.head: return print('My linked list is empty')
+        else: 
+            itr = self.head
+            while itr:
+                itr = itr.next
+
+if __name__ == "__main__":
+    pass
