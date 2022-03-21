f439f58af4460a948d0f1d078ba8c2e9e066c360
kramstyles <mark.eke@decagon.dev>
2022-03-21 20:35:04 +0100

    added the reomove at index function

--- Python/linked_lists_2.py
+++ Python/linked_lists_2.py
@@ -48,24 +48,54 @@ class LinkedPersons:
         persons.reverse()
         self.place_people_at_back(persons)
 
+    def reverse_people_without_list(self):
+        # Todo: Runs into infinite loop.. Fix issue
+        while self.front:
+            self.place_person_at_back(self.front.front_person)
+            self.front = self.front.next_person
+
     def number_of_people(self):
         people = 0
-        while self.front:
+        current_people = self.front
+        while current_people:
             people += 1
+            current_people = current_people.next_person
+        return people
+
+    def remove_person_at(self, position: int):
+        if position < 0 or position >= self.number_of_people():
+            return print("Entry is invalid")
+        elif position == 0:
             self.front = self.front.next_person
-        return print('Number of people:', people)
+        else:
+            counter = 0
+            current_pos = self.front
+            while current_pos:
+                if counter == position - 1:
+                    current_pos.next_person = current_pos.next_person.next_person
+                    break
+                current_pos = current_pos.next_person
+                counter += 1
 
 
 if __name__ == '__main__':
     queue = LinkedPersons()
+
     queue.list_persons()
     queue.place_person_at_back('Ola')
     queue.place_person_in_front('Jeremiah')
     queue.place_person_in_front('Michelle')
     queue.place_person_at_back('Donald')
+
     queue.list_persons()
     queue.place_people_at_back(['Folu', 'Ebubue', 'Daniel', 'Okro'])
+
     queue.list_persons()
     queue.reverse_people()
+
     queue.list_persons()
-    queue.number_of_people()
+    print("Number of people in queue:", queue.number_of_people())
+
+    queue.remove_person_at(0)  # Remove okro
+    queue.remove_person_at(2)  # Remove folu
+    queue.list_persons()
