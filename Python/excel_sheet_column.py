c6e51dc9cd3abd264d6cf6cd732016b6062de88d
kramstyles <mark.eke@decagon.dev>
2022-03-21 10:30:56 +0100

    fixed for single chars

--- Python/excel_sheet_column.py
+++ Python/excel_sheet_column.py
@@ -1,2 +1,13 @@
-def solution():
-    pass
+def solution(columnTitle: str) -> int:
+    # Generate all the alphabets in uppercase
+    from string import ascii_uppercase as alpha
+
+    if len(columnTitle) > 1:
+        for char in columnTitle:
+            print(alpha.index(char) + 1)
+    else:
+        return alpha.index(columnTitle) + 1
+    
+    
+
+solution('AB')
