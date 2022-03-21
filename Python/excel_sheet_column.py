698d640d6c7dbda4c48c47a6c47b7c59124dbd9a
kramstyles <mark.eke@decagon.dev>
2022-03-21 10:43:56 +0100

    first test excel_sheet.py

--- Python/excel_sheet_column.py
+++ Python/excel_sheet_column.py
@@ -3,11 +3,19 @@ def solution(columnTitle: str) -> int:
     from string import ascii_uppercase as alpha
 
     if len(columnTitle) > 1:
-        for char in columnTitle:
-            print(alpha.index(char) + 1)
+        # To get the index of each character
+        total = [alpha.index(char) + 1 for char in columnTitle]
+        
+        total.reverse()
+        for x in range(len(total)):
+            total[x] += (x * 26)
+        return sum(total) - 1
     else:
         return alpha.index(columnTitle) + 1
     
     
 
-solution('AB')
+print(solution('AB'))
+print(solution('A'))
+print(solution('Z'))
+print(solution('ZY'))
