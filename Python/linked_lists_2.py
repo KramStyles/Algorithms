8069f2a4414fbbebcef0f72dcd0b0d1977c63dac
kramstyles <mark.eke@decagon.dev>
2022-03-21 18:31:05 +0100

    printed out list of people

--- Python/linked_lists_2.py
+++ Python/linked_lists_2.py
@@ -17,9 +17,20 @@ class LinkedPersons:
             new_person = Node(person, self.front)
             self.front = new_person
 
+    def list_persons(self):
+        if not self.front:
+            print('No one is in the queue')
+        else:
+            current_person = self.front
+            while current_person:
+                print(current_person.front_person, '--≥')
+                current_person = current_person.next_person
+
 
 if __name__ == '__main__':
     queue = LinkedPersons()
+    queue.list_persons()
     queue.place_person_at_back('Ola')
     queue.place_person_in_front('Jeremiah')
     queue.place_person_in_front('Michelle')
+    queue.list_persons()
