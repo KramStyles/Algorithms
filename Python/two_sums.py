a7ebcda421c37c8110fdcca14d334a14239a16f2
kramstyles <mark.eke@decagon.dev>
2022-03-23 17:10:43 +0100

    implemented two pointer for two_sums.py

--- Python/two_sums.py
+++ Python/two_sums.py
@@ -1,3 +1,5 @@
+from variables import large_two_sums
+
 def solution(numbers: list[int], target: int) -> list[int]:
     """
     Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
@@ -8,7 +10,12 @@ def solution(numbers: list[int], target: int) -> list[int]:
 
     The tests are generated such that there is exactly one solution. 
     You may not use the same element twice.
-
+        p=len(numbers)-1
+        for i, n in enumerate(numbers):
+        while p>i and numbers[p]>target-n:
+        p-=1
+        if numbers[p2]==target-n:
+        return [i+1,p+1]
 
     Your solution must use only constant extra space.   
     """
@@ -22,7 +29,22 @@ def solution(numbers: list[int], target: int) -> list[int]:
             if number + num == target:
                 return [index +  1, numbers.index(num, index + 1) + 1]
 
+def solution_two(numbers: list[int], target: int) -> list[int]:
+    front = 0
+    back = len(numbers)-1
+    while front < back:
+        check = numbers[front] + numbers[back]
+        if check < target:
+            front += 1
+        elif check > target:
+            back -= 1
+        else:
+            return [front + 1, back + 1]
+    return -1
+            
 
-print(solution(numbers = [2,7,11,15], target = 9)) # [1, 2]
-print(solution(numbers = [2,3,4], target = 6)) # [1, 3]
-print(solution(numbers = [0,0,3,4], target = 0)) # [1, 2]
+
+print(solution_two(numbers = [2,7,11,15], target = 9)) # [1, 2]
+print(solution_two(numbers = [2,3,4], target = 6)) # [1, 3]
+print(solution_two(numbers = [0,0,3,4], target = 0)) # [1, 2]
+print(solution_two(large_two_sums, 5))
