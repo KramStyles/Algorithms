a243eae37b6576fdef86b7e78944ed3990a33c1b
kramstyles <mark.eke@decagon.dev>
2022-03-27 01:19:35 +0100

    added super reduced strings and solved

---
+++ Python/super_reduced_strings.py
@@ -0,0 +1,45 @@
+import enum
+
+
+def superReducedStrings(s):
+    """
+        Reduce a string of lowercase characters in range ascii[‘a’..’z’]by doing a series of operations. 
+        In each operation, select a pair of adjacent letters that match, and delete them.
+
+        Delete as many characters as possible using this method and return the resulting string.
+        If the final string is empty, return Empty String
+
+        Example.
+
+        aab shortens to b in one operation: remove the adjacent a characters.
+        Remove the two 'b' characters leaving 'aa'. Remove the two 'a' characters to leave ''. 
+        Return 'Empty String'.
+
+        Function Description
+        Complete the superReducedString function in the editor below.
+
+        aaabccddd → abccddd → abddd → abd
+            
+    """
+    from collections import Counter
+    s = list(s)
+    counts = Counter(s).values()
+    max_of_each = max(counts)
+
+    while max_of_each > 1:
+        for index, char in enumerate(s):
+            if index < len(s):
+                next = s[index+1]
+                if char == next:
+                    s.pop(index)
+                    s.pop(index)
+                    break;
+        
+        counts = Counter(s).values()
+        max_of_each = max(counts)
+
+
+    return ''.join(s)
+    
+# superReducedStrings('aaabccddd')
+superReducedStrings('aa')
