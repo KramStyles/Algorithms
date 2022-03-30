6fea7ee42ff7bbc0e805315ff4a9533718cdf27e
kramstyles <mark.eke@decagon.dev>
2022-03-30 13:23:29 +0100

    reversed string using stack

--- Python/stack.py
+++ Python/stack.py
@@ -2,19 +2,37 @@ from collections import deque
 
 class Stack:
     def __init__(self) -> None:
-        self.stack = deque
+        self.stack = deque()
 
     def push(self, val):
         self.stack.append(val)
 
     def pop(self):
-        return self.stack.pop()
+        if not self.is_empty: return self.stack.pop()
+        else: return print('Cannot remove from an empty Stack')
+
 
     def peek(self):
         return self.stack[-1]
 
+    @property
     def is_empty(self):
         return len(self.stack) == 0
 
     def size(self):
         return len(self.stack)
+
+s = Stack()
+s.push('Mental')
+s.push('Dental')
+s.pop()
+
+def reverse_string(word):
+    t = Stack()
+    for char in word: t.push(char)
+    ans = ''
+    while t.stack:
+        ans += t.pop()
+    return ans
+
+reverse_string('we will conquer')
