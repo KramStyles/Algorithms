3683b14b0d71fda0beb7ffc783ac5f5e19d5963d
kramstyles <mark.eke@decagon.dev>
2022-03-30 12:29:54 +0100

    added stack.py

---
+++ Python/hash_tables_2.py
@@ -0,0 +1,22 @@
+class HashTable:
+    def __init__(self, size) -> None:
+        self.size = size
+        self.memory = [None for item in range(self.size)]
+
+    def get_hash(self, word):
+        return sum([ord(item) for item in word]) % self.size
+
+    def add(self, word, value):
+        key = self.get_hash(word)
+        self.memory[key] = value
+
+    def get(self, word):
+        key = self.get_hash(word)
+        return self.memory[key]
+
+
+
+ht = HashTable(10)
+ht.add('name', 'michael')
+ht.get('nae')
+ht.memory
---
+++ Python/stack.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Stack:
+    def __init__(self) -> None:
+        self.stack = deque
+
+    def push(self, val):
+        self.stack.append(val)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
