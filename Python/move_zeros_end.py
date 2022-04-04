183e7ce4f236cda1ff9748691752c1f70e3f9e05
kramstyles <mark.eke@decagon.dev>
2022-04-04 02:13:07 +0100

    finished move_zeros

---
+++ Python/move_zeros_end.py
@@ -0,0 +1,22 @@
+def move_zeros(array):
+    """
+    Write an algorithm that takes an array 
+    and moves all of the zeros to the end, 
+    preserving the order of the other elements. 
+    Took me from 191 to 244. Kata 5
+
+    First algorithm i wrote and it passed all test at once. Wonderful
+    """
+    zeros = array.count(0)
+    if zeros:
+        while zeros > 0:
+            for idx, dig in enumerate(array):
+                if dig == 0:
+                    array.remove(dig)
+                    array.insert(len(array), dig)
+                    zeros -= 1
+                    break
+    return array
+
+
+move_zeros([1, 0, 1, 2, 0, 1, 3])
