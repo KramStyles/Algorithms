754dc1c2513ba91389a57e6aa820df4627e5745a
kramstyles <mark.eke@decagon.dev>
2022-04-12 11:43:33 +0100

    shorter and better version of position problem.py

--- Python/interview/python_2_position_problem.py
+++ Python/interview/python_2_position_problem.py
@@ -3,6 +3,7 @@ def solution(arr, target):
     Given a sorted array of integers arr and an integer target, find the index 
     of the first and last position of target in arr. If target can't be found,
     return '-1'
+    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/
     """
     if target not in arr:
         return -1
@@ -12,12 +13,15 @@ def solution(arr, target):
             if item == target:
                 start = idx
                 stop = idx
-                if (idx + 1) < len(arr):
-                    while arr[idx+1] == item:
-                        stop += 1
-                        idx += 1
-                        if(idx + 1) >= len(arr): break
-                return [start, stop]
+                while idx + 1 < len(arr) and arr[idx + 1] == target:
+                    idx += 1
+                return [start, idx]
+                # if (idx + 1) < len(arr):
+                #     while arr[idx+1] == item:
+                #         stop += 1
+                #         idx += 1
+                #         if(idx + 1) >= len(arr): break
+                # return [start, stop]
 
 solution([2,4,5,5,5,5,5,7,9,9], 5)
 solution([1], 1)
