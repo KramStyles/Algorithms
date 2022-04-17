4a4ad2285f38a95c42e0e100a51b6b931c0ed980
kramstyles <mark.eke@decagon.dev>
2022-04-17 14:27:41 +0100

    created code for bubble sort

--- Python/sorting/bubble.py
+++ Python/sorting/bubble.py
@@ -4,4 +4,13 @@ def bubble(unsorted_list):
     while not sort:
         sort = True
         for num in range(0, list_len):
-            
+            numb1 = unsorted_list[num]
+            numb2 = unsorted_list[num+1]
+            if numb1 > numb2:
+                sort = False
+                unsorted_list[num], unsorted_list[num+1] = numb2, numb1
+
+    return unsorted_list
+
+bubble([12, 1, 43, 29, 11, 23, 4, 21, 45, 7])
+
