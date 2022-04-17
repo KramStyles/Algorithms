3ed6ebba32eaf0b9056f989f2942d8462b706d98
kramstyles <mark.eke@decagon.dev>
2022-04-17 20:53:07 +0100

    solved turing test questions

---
+++ Python/interview/turing_test1.py
@@ -0,0 +1,46 @@
+def calPoints(ops):
+    """
+    Add number to list, 
+    C => removes previous score
+    D => Add 2 * previous
+    + => Sum of previous 2 scores
+    """
+    result = []
+    for num in ops:
+        if num == 'C':
+            if result != []:
+                result.pop()
+        elif num == 'D':
+            if result != []:
+                result.append(result[-1] * 2)
+        elif num == '+':
+            if result != []:
+                result.append(sum(result[-2:]))
+        else:
+            result.append(int(num))
+
+    
+    return sum(result)
+
+# calPoints(["5", "-2", "4", "C", "D", "9", "+", "+"]) # 27
+
+def checkBrackets(s):
+    oppo = {
+        '{': '}',
+        '[': ']', 
+        '(': ')',
+        '}': '{',
+        ']': '[', 
+        ')': '('
+    }
+    group = []
+    for x in s:
+        if len(group) > 0 and oppo[group[-1]] == x:
+            group.pop()
+        else:
+            group.append(x)
+    # groups = [group.pop() if len(group) > 0 and oppo[group[-1]] == x else group.append(x) for x in s]
+    if not group: return print('Valid')
+    return print('Invalid')
+
+checkBrackets('()[]{}{{}}')
