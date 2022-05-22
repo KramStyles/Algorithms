0ecbe7af45531a8f0e885797e3df189fe438ebcf
kramstyles <mark.eke@decagon.dev>
2022-05-22 13:26:30 +0100

    fixed leet baseball algo

---
+++ Python/leet/baseball.py
@@ -0,0 +1,19 @@
+class Solution:
+    def calPoints(self, ops: list[str]) -> int:
+        total = []
+        for item in ops:
+            if item.isdigit() or item[0] == '-':
+                total.append(int(item))
+            elif item == '+':
+                total.append(total[-1] + total[-2])
+            elif item == 'D':
+                total.append(total[-1] * 2)
+            elif item == 'C':
+                total.pop()
+        return sum(total)
+
+
+test2 = ["5","-2","4","C","D","9","+","+"]
+                
+sol = Solution()
+sol.calPoints(test2)
