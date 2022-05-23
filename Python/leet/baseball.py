028ca44a4bd26ae03e39b843f7466c59f83f7df6
kramstyles <mark.eke@decagon.dev>
2022-05-23 21:09:08 +0100

    edited

--- Python/leet/baseball.py
+++ Python/leet/baseball.py
@@ -11,7 +11,7 @@ class Solution:
             elif item == 'C':
                 total.pop()
         return sum(total)
-
+    # Just putting code here to meet the requirement for today
 
 test2 = ["5","-2","4","C","D","9","+","+"]
                 
