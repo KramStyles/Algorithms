cf47a525c26d659d82b1e49538841ca756004297
kramstyles <mark.eke@decagon.dev>
2022-06-08 00:17:39 +0100

    solved house robbery with simple step

--- Python/dynamic_programming/house_robber.py
+++ Python/dynamic_programming/house_robber.py
@@ -22,18 +22,27 @@ class Solution:
         Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
         Total amount you can rob = 2 + 9 + 1 = 12.
         """
-        total = [0, 0]
-        for item in range(0, len(nums), 2):
-            total[0] += nums[item]
-        for item in range(1, len(nums), 2):
-            total[1] += nums[item]
-        return print(max(total))
+        # total = [0, 0]
+        # for item in range(0, len(nums), 2):
+        #     total[0] += nums[item]
+        # for item in range(1, len(nums), 2):
+        #     total[1] += nums[item]
+        # return print(max(total))
+
+        prev , nxt = 0, 0
+        for num in nums:
+            temp = max(num + prev, nxt)
+            prev = nxt
+            nxt = temp
+        
+        return nxt
 
 
 soln = Solution()
 nums = [1, 2]
 nums1 = [1,2,3,1] # 4
 nums2 = [2,7,9,3,1] # 12
+nums2 = [2,1,1,2] # 4
 
 soln.rob(nums1)
 soln.rob(nums2)
