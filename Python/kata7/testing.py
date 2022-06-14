38f945f0b632d7aeff3be2fb05c4823a9a9181dd
kramstyles <mark.eke@decagon.dev>
2022-06-14 19:49:08 +0100

    worked on testing 1-2-3

---
+++ Python/kata7/testing.py
@@ -0,0 +1,14 @@
+def number(lines):
+    """
+    Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+    Write a function which takes a list of strings and returns each line prepended by the correct number.
+    The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+    Examples: (Input --> Output)
+
+    [] --> []
+    ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+    """
+    return [f"{index + 1}: {item}" for index, item in enumerate(lines)]
+
+print(number(['a', 'b', 'c']))
