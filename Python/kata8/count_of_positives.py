7a9e1ea547513fe41e8a20447cb8b2434bec1e73
kramstyles <mark.eke@decagon.dev>
2022-08-28 21:18:02 +0100

    added count_of_numbers.py

---
+++ Python/kata8/count_of_positives.py
@@ -0,0 +1,15 @@
+def counts(arr):
+    """
+    Return an array, where the first element is the count of positives numbers
+    and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+    If the input is an empty array or is null, return an empty array.
+    """
+    res = []
+    for num in arr:
+        if num > 0:
+            res[0] += 1
+        elif num < 0:
+            res[1] += num
+
+    return res
