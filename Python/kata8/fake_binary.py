67a6b7d6c5cc65c22881a2aece3d2e662e33f5d2
kramstyles <mark.eke@decagon.dev>
2022-08-29 20:37:41 +0100

    solving fake_binary with 1 liner

---
+++ Python/kata8/fake_binary.py
@@ -0,0 +1,8 @@
+def fake_bin(x):
+    """
+    Given a string of digits, you should replace any digit below 5 with '0'
+    and any digit 5 and above with '1'. Return the resulting string.
+
+    Note: input will never be an empty string
+    """
+    return "".join([0 if int(dig) < 5 else 1 for dig in x])    
