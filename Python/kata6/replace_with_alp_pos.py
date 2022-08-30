47e8ea2e1544c96a2fdfd486cacf73445364f41d
kramstyles <mark.eke@decagon.dev>
2022-08-30 20:58:14 +0100

    added alphabet_position

---
+++ Python/kata6/replace_with_alp_pos.py
@@ -0,0 +1,8 @@
+def alphabet_position(text):
+    """
+    In this kata you are required to, given a string, 
+    replace every letter with its position in the alphabet.
+
+    If anything in the text isn't a letter, ignore it and don't return it.
+    """
+    pass
