c7f54e421967dd1040088e11ee0c1d217324a352
kramstyles <mark.eke@decagon.dev>
2022-08-30 22:03:33 +0100

    solved alphabet position

--- Python/kata6/replace_with_alp_pos.py
+++ Python/kata6/replace_with_alp_pos.py
@@ -1,3 +1,9 @@
+from string import ascii_lowercase
+
+pos = {}
+for idx, item in enumerate(ascii_lowercase):
+    pos[item] = str(idx + 1)
+
 def alphabet_position(text):
     """
     In this kata you are required to, given a string, 
@@ -5,4 +11,6 @@ def alphabet_position(text):
 
     If anything in the text isn't a letter, ignore it and don't return it.
     """
-    pass
+    return ''.join([f"{pos[item.lower()]} " for item in text if item.lower() in pos])
+
+print(alphabet_position("The sunset sets at twelve o' clock."))
