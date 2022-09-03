739247e6d7b5a8326f44859e5aa7845134d701c6
kramstyles <mark.eke@decagon.dev>
2022-09-03 20:44:25 +0100

    fixed sentence_smash

---
+++ Python/kata8/sentence_smash.py
@@ -0,0 +1,10 @@
+def _smash(words):
+    """
+    Write a function that takes an array of words and smashes them together into a 
+    sentence and returns the sentence. You can ignore any need to sanitize words or
+    add punctuation, but you should add spaces between each word. Be careful, 
+    there shouldn't be a space at the beginning or the end of the sentence!
+    ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+    """
+    
+    return ' '.join(word for word in words).strip()
