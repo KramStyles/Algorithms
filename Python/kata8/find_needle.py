3d451ed910a87fe7cec8984aeca63fb70c2e3002
kramstyles <mark.eke@decagon.dev>
2022-09-04 22:27:11 +0100

    added needle in haystack

---
+++ Python/kata8/find_needle.py
@@ -0,0 +1,7 @@
+def find_needle(haystack:list):
+    """
+    Write a function findNeedle() that takes an array full of junk but containing one "needle"
+    After your function finds the needle it should return a message (as a string) that says:
+    "found the needle at position " plus the index it found the needle, so:
+    """
+    return f"found the needle at position {haystack.index('needle')}"
