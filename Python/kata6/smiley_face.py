17852ff4a34c56e91807ebd67520fc872a4943b5
kramstyles <mark.eke@decagon.dev>
2022-09-04 22:31:52 +0100

    added query for smileyface

---
+++ Python/kata6/smiley_face.py
@@ -0,0 +1,16 @@
+def count_smileys(arr):
+    """
+    Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+    Rules for a smiling face:
+
+    - Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+    - A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+    - Every smiling face must have a smiling mouth that should be marked with either ) or D
+    No additional characters are allowed except for those mentioned.
+
+    Valid smiley face examples: :) :D ;-D :~)
+    Invalid smiley faces: ;( :> :} :]
+
+    return the number of valid smiley faces in array/list
+    """
