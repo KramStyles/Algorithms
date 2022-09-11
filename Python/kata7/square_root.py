141711b900c199a6aa91cd15430b49d0b8a89f66
kramstyles <mark.eke@decagon.dev>
2022-09-11 20:11:45 +0100

    solved perfect square without math import

--- Python/kata7/square_root.py
+++ Python/kata7/square_root.py
@@ -2,3 +2,6 @@ def is_square(n):
     import math
     # This takes in a number and determines if it's a perfect square  
     return True if int(math.sqrt(abs(n)))**2 == n else False
+
+def is_square_without_math_import(n):
+    return n >= 0 and (n**0.5) % 1 == 0
