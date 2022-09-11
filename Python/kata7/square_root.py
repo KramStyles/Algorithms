4b211559dc34fdd68d55a27c050a3b94ff9f8e32
kramstyles <mark.eke@decagon.dev>
2022-09-11 20:11:00 +0100

    solved perfect square

--- Python/kata7/square_root.py
+++ Python/kata7/square_root.py
@@ -1,4 +1,4 @@
 def is_square(n):
     import math
     # This takes in a number and determines if it's a perfect square  
-    return True if not math.sqrt(abs(n)) % 2 else False
+    return True if int(math.sqrt(abs(n)))**2 == n else False
