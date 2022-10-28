883c6daa95d74a6119fb4cebd1e5cf93402db925
kramstyles <mark.eke@decagon.dev>
2022-10-28 16:36:58 +0100

    finished signature nth on both functions

--- Python/signature_nth_rarest_item.py
+++ Python/signature_nth_rarest_item.py
@@ -19,11 +19,13 @@ def nth_most_rate(array: list, n: int):
 
     from collections import Counter
     items = Counter(array)
+    # items would contain each item in the list and the number of times they've occured saved in a dictionary
+    # e.g {5: 5, 4: 4, 3: 3, 2: 2, 1: 1}
 
     return items[n]
 
 
-def nth_most_rate2():
+def nth_most_rate2(array: list, n: int):
     """
     This function goes through the list and finds the nth rarest item (n)
     params
@@ -31,10 +33,22 @@ def nth_most_rate2():
     n: (int) => Nth rare item
     return result: (int) Nth rarest item from the array
     """
-    pass
+    # First I would covert the array to a set to have only distinct values then run a loop
+    # to find the nth rarest item. This helps the big O notation so as it maintains O(n)
+
+    distinct_array = set(array)
+
+    # Here we loop through the distinct array
+    for item in distinct_array:
+        number = array.count(item)
+        if number == n:
+            return item
+
+    return None
 
 
 array = [5, 4, 5, 4, 5, 4, 4, 5, 3, 3, 3, 2, 2, 1, 5]
 n = 2
 
-nth_most_rate(array, 7)
+nth_most_rate(array, n)
+nth_most_rate2(array, n)
