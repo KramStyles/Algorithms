adbcece32586bbaa85bc927449cad18680d9d004
kramstyles <mark.eke@decagon.dev>
2022-10-28 16:16:37 +0100

    created signature nth rarest item.py with first solution

---
+++ Python/signature_nth_rarest_item.py
@@ -0,0 +1,27 @@
+"""
+Given a list of integers, create a function that returns the nth-rarest item. 
+The function should be called nth_most_rare signature and its signarture nth_most_rate signature (list, n)
+where list is the array of integers and n is the nth rarest term. 
+
+For example in ([5,4,5,4,5,4,4,5,3,3,3,2,2,1,5]), if 2 is supplied as n, the answer is 2 as 2 is the 2nd rarest item
+"""
+
+
+def nth_most_rate(array: list, n: int):
+    """
+    This function goes through the list and finds the nth rarest item (n)
+    params
+    array: (list) => List of integers
+    n: (int) => Nth rare item
+    return result: (int) Nth rarest item from the array
+    """
+    from collections import Counter
+    items = Counter(array)
+
+    print(items[2])
+
+
+array = [5, 4, 5, 4, 5, 4, 4, 5, 3, 3, 3, 2, 2, 1, 5]
+n = 2
+
+nth_most_rate(array, n)
