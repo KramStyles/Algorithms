a22ec8715ae8aa2bffb2d593163470c8880dc2a2
kramstyles <kramstyles@outlook.com>
2023-03-14 23:41:41 +0100

    solved simple boolean to string

---
+++ Python/kata8/boolean_to_string.py
@@ -0,0 +1,13 @@
+def boolean_to_string(b):
+    """
+    Implement a function which convert the given boolean value into its string representation.
+
+    Note: Only valid inputs will be given.
+    """
+
+    return 'True' if b else 'False'
+
+
+print(boolean_to_string(True))
+print(boolean_to_string(False))
+print(boolean_to_string(False))
