55a85f65ff136a682ed475b621b9f39da35441b0
kramstyles <kramstyles@outlook.com>
2023-03-27 22:07:16 +0100

    solved pangram

--- Python/kata6/detect_pangram.py
+++ Python/kata6/detect_pangram.py
@@ -1,4 +1,4 @@
-def is_pangram(s):
+def is_pangram(sentence):
     """
     A pangram is a sentence that contains every single letter of the alphabet 
     at least once. For example, the sentence "The quick brown fox jumps over 
@@ -8,4 +8,20 @@ def is_pangram(s):
     Given a string, detect whether or not it is a pangram. Return True if it 
     is, False if not. Ignore numbers and punctuation.
     """
-    return False
+    # Import lowercase characters
+    from string import ascii_lowercase
+
+    # Convert sentence to lowercase
+    sentence = sentence.lower()
+    
+    # loop through alphabets and end loop if it doesn't exist
+    for char in ascii_lowercase:
+        if char not in sentence:
+            return False
+
+    return True
+
+sentence1 = "The quick, brown fox jumps over the lazy dog!"
+sentence2 = "1bcdefghijklmnopqrstuvwxyz"
+print(is_pangram(sentence1))
+print(is_pangram(sentence2))
